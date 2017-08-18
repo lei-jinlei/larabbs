ad4bab9e9d6df58fea1668d32ed4a6f61940120f
雷金磊 <leijinlei_phper@163.com>
2017-08-18 15:43:10 +0800

    添加几个常用函数

--- fun_1.php
+++ fun_1.php
@@ -35,14 +35,97 @@ filter_var($email,FILTER_VALIDATE_EMAIL);
 // 验证url
 filter_var($url,FILTER_VALIDATE_URL);
 
+/**
+ * 创建多级目录
+ * @param  [type]  $path 要创建的目录
+ * @param  integer $mode 创建目录的模式，在Windows下省略
+ * @return [type]        是否成功
+ */
+function create_dir($path, $mode = 0777)
+{
+    if (is_dir($path)) {
+        # 如果目录已经存在，则不创建
+        echo '该目录已经存在';
+    } else {
+        # 不存在，创建
+        if (mkdir($path, $model, true)) {
+            echo '创建成功';
+        } else {
+            echo '创建失败';
+        }
+    }
+}
 
 
+// php用preg_match来匹配并判断一个字符串中是否含有中文或者都是中文的方法
+function is_chinese_str($str)
+{
+    if (preg_match('/[\x7f-\xff]/', $str)) {
+        echo '字符串中有中文';
+    } else {
+        echo '字符串中没有中文';
+    }
+}
 
+// 确保多个进程同时写入同一个文件成功
+$fp = fopen('lock.txt', "w+");
+if (flock($fp, LOCK_EX)) {
+    // 获得写锁，写数据
+    fwrite($fp, "write something");
+    // 解除锁定
+    flock($fp, LOCK_UN);
+} else {
+    echo 'file is locking...';
+}
+fclose($fp);
 
+// 取出url扩展名
+function getExt($url)
+{
+    $arr = parse_url($url);
+    $file = basename($arr['path']);
+    $ext = explode('.', $file);
+    return $ext[count($ext)-1];
+}
 
+// 取出url扩展名
+function getExt_2($url)
+{
+    $url = basename($url);
+    $pos1 = strpos($url, '.');
+    $pos2 = strpos($url, '?');
+    if (strstr($url, '?')) {
+        return substr($url, $pos1+1, $pos2-$pos1-1);
+    } else {
+        return substr($url, $pos1);
+    }
+}
 
+// 遍历一个文件夹下的所有文件和子文件夹
+function my_scandir($dir)
+{
+    $files = array();
+    if (is_dir($dir)) {
+        if ($handle = opendir($dir)) {
+            while (($file = readdir($handle)) !== false) {
+                if ($file != "." && $file != "..") {
+                    if (is_dir($dir . '/' . $file)) {
+                        $files[$file] = my_scandir($dir . '/'. $file);
+                    } else {
+                        $files[] = $dir . '/'. $file;
+                    }
+                }
+            }
+            closedir($handle);
+            return $files;
+        }
+    }
+}
 
-
+// 判断一个字符串是否为utf-8格式
+if (mb_detect_encoding($str, 'UTF-8', true)) {
+    // 是UTF-8格式的字符
+}
 
 
 
