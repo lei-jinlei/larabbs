e67f9aa0a4f838d605a135e10af8636cd52580fb
雷金磊 <leijinlei_phper@163.com>
2018-01-28 19:02:15 +0800

    用户发布的话题
    用户发布的话题

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -11,6 +11,11 @@
 
                 <div class="media-body">
 
+                    <a href="{{ route('categories.show', $topic->category_id) }}" title="{{ $topic->category->name }}">
+                        <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+                        {{ $topic->category->name }}
+                    </a>
+                    
                     <div class="media-heading">
                         <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
                             {{ $topic->title }}
