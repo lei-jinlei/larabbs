5a7a95c9775d50d77f5dd7433b097a9671d8754f
雷金磊 <leijinlei_phper@163.com>
2018-03-27 21:19:08 +0800

    获取个人信息
     获取个人信息

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Transformers\UserTransformer;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\Api\UserRequest;
@@ -31,5 +32,17 @@ class UsersController extends Controller
         \Cache::forget($request->verification_key);
 
         return $this->response->created();
+    }
+
+    public function me()
+    {
+        $user = $this->user();
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
     }
 }
