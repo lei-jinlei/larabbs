f1216ce9fb8b8ccc8945f2a2e7ab5dbd968e61c8
雷金磊 <leijinlei_phper@163.com>
2018-04-08 22:21:12 +0800

    回复，消息通知相关的接口
    回复，消息通知相关的接口

--- routes/api.php
+++ routes/api.php
@@ -87,6 +87,35 @@ $api->version('v1', [
                 ->name('api.topics.update');
             $api->delete('topics/{topic}', 'TopicsController@destroy')
                 ->name('api.topics.destroy');
+
+            // 发布回复
+            $api->post('topics/{topic}/replies', 'RepliesController@store')
+                ->name('api.topics.replies.store');
+            // 删除回复
+            $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
+                ->name('api.topics.replies.destroy');
+
+            // 通知列表
+            $api->get('user/notifications', 'NotificationsController@index')
+                ->name('api.user.notifications.index');
+            // 通知统计
+            $api->get('user/notifications/stats', 'NotificationsController@stats')
+                ->name('api.user.notifications.stats');
+            // 标记消息通知为已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')
+                ->name('api.user.notifications.read');
+
+            // 某个用户发布的话题
+            $api->get('users/{user}/topics', 'TopicsController@userIndex')
+                ->name('api.users.topics.index');
+            // 话题回复列表
+            $api->get('topics/{topic}/replies', 'RepliesController@index')
+                ->name('api.topics.replies.index');
+            // 某个用户的回复列表
+            $api->get('users/{user}/replies', 'RepliesController@userIndex')
+                ->name('api.users.replies.index');
+
+
         });
     });
 });
